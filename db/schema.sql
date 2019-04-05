f0e95a4ee54a0a2e9900e692730ab370ce4ca4d9
Kyle Babcock <kylerbabcock@gmail.com>
2019-04-05 19:19:38 -0400

    sql files created and connection established

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+create database rollercoaster_db;
+use rollercoaster_db;
+
+create table coasters (
+    id int(10) auto_increment not null,
+    coaster_name varchar(255) not null,
+    rode boolean default false,
+    primary key(1d)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+insert into coasters (coaster_name)
+values ("Mako"), ("Millennium Force"), ("Banshee");
