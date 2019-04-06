9118dd73b41cc5d98bbce9f5bfd0e9fdde88d35e
Kyle Babcock <kylerbabcock@gmail.com>
2019-04-05 20:43:15 -0400

    added server and first function is posting to page

---
+++ controllers/controller.js
@@ -0,0 +1,17 @@
+var coasters = require("../models/coaster.js");
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    coasters.ridden(function(data) {
+        var object = {
+            coasters: data
+        };
+        res.render("index", object);
+    })
+})
+
+
+
+
+module.exports = router;
